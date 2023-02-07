295be48aabaacdfd6b8bc00710dc932b637de332
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-08 03:29:32 +0500

    add JS assignment (5) questions

---
+++ smit assignment 17/app.js
@@ -0,0 +1,90 @@
+// chapter # 9-11
+
+// // Qno# 1
+
+// var city =prompt("enter your city")
+// if(city === "karachi"){
+//     alert("welcome to city of lights...")
+// }
+
+
+// // Qno# 2
+
+// var Gender = prompt("enter your Gender")
+// if(Gender === "male"){
+//     alert("Good morning Sir ")
+// }
+// if(Gender === "female"){
+//     alert("Good morning Ma'am")
+// }
+
+
+// // Qno# 3
+
+// var lights = prompt("Signal color")
+// if(lights === "red"){
+//     alert("Must stop")
+    
+// }
+// if( lights === "yellow"){
+//     alert("Ready to Move")
+
+// }
+// if(lights === "green"){
+//     alert("Move now")
+// }    
+
+
+// // Qno# 4
+
+// var petrol = +prompt("enter your remaining fuel(liters)")
+// if(petrol <= "0.25"){
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// // Qno# 5
+
+// // a
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// // b
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// // c
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+//     if (c === 13){
+//     alert("condition 2 is true");
+//     }
+//     if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+//     if(c === 14){
+//     alert("condition 4 is true");
+//     }
+
+// // d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The total cost is" +" "+ totalCost);
+// }
+
+
+// // e
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
