21b64d1a3248c67785f9594c86fd63bd2965cc9c
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-09 03:56:51 +0500

    complete assignment 4 Chapter 9-11

---
+++ smit assignment 4/app.js
@@ -0,0 +1,190 @@
+// chapter # 9-11
+
+// // Qno# 1
+
+// var city =prompt("enter your city")
+// if(city === "karachi"){
+//     alert("welcome to city of lights...")
+// }
+
+
+// // Qno# 2
+
+// var Gender = prompt("enter your Gender")
+// if(Gender === "male"){
+//     alert("Good morning Sir ")
+// }
+// if(Gender === "female"){
+//     alert("Good morning Ma'am")
+// }
+
+
+// // Qno# 3
+
+// var lights = prompt("Signal color")
+// if(lights === "red"){
+//     alert("Must stop")
+    
+// }
+// if( lights === "yellow"){
+//     alert("Ready to Move")
+
+// }
+// if(lights === "green"){
+//     alert("Move now")
+// }    
+
+
+// // Qno# 4
+
+// var petrol = +prompt("enter your remaining fuel(liters)")
+// if(petrol <= "0.25"){
+//     alert("Please refill the fuel in your car")
+// }
+
+
+// // Qno# 5
+
+// // a
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// // b
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// // c
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+//     if (c === 13){
+//     alert("condition 2 is true");
+//     }
+//     if (++c < 14){
+//     alert("condition 3 is true");
+//     }
+//     if(c === 14){
+//     alert("condition 4 is true");
+//     }
+
+// // d
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The total cost is" +" "+ totalCost);
+// }
+
+
+// // e
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// // Qno# 6
+
+// var obtMarks = prompt("enter your obtain marks")
+// var totalMarks = 300
+// var per = obtMarks / totalMarks * 100
+// if(per < 60){
+// document.write("fail")
+// }
+// else if(per >= 60 && per <= 70){
+//     document.write("you need improvemet")
+// }
+
+// else if(per >= 70 && per <= 80){
+//     document.write("Good")
+// }
+
+// else if(per >= 80 && per <= 100){
+//     document.write("excellent")
+// }
+
+
+//  // Qno# 7
+
+// var secNum = 7
+// var guess = +prompt("guess your number (1-10)")
+
+
+// if(guess === secNum){
+//     alert("bingo! Correct answer")
+// }
+// else if(guess === secNum + 1 || guess === secNum - 1){
+//     alert("your are close ")
+
+// }
+// else{
+//     alert("try again")
+// }
+
+
+// // Qno# 8
+
+
+// var num = prompt("enter your num")
+// var div = num % 3
+// if(div === 0){
+//     alert("your number is divisible by 3")
+// }
+// else{
+//      alert("your number is not divisible by 3")
+// }
+
+//  // Qno# 9
+
+// var num = prompt("enter your num")
+// var even = num %  2
+
+// if(even === 0){
+//     alert("YOUR NUM IS EVEN")
+// }
+// else{
+//     alert("your num is odd  ")
+// }
+
+// // Qno# 10
+
+// var temp = +prompt("Enter temperature")
+// if(temp > 40){
+//     alert("It is too hot outside.")
+// }
+// else if(temp > 30){
+//     alert("The Weather today is Normal.")
+// }
+// else if(temp > 20){
+//     alert("Today's Weather is cool.")
+// }
+// else if(temp > 10){
+//     alert("OMG! Today's weather is so Cool.")
+// }
+
+// // Qno# 11
+
+// var first = +prompt("enter your num")
+// var oprt = prompt("Enter your operator")
+// var second = +prompt("enter your num")
+// if(oprt === "+"){
+//     alert(first + second)
+// }
+// else if(oprt === "-"){
+//     alert(first - second)
+// }
+// else if(oprt === "*"){
+//     alert(first * second)
+// }
+// else if(oprt === "/"){
+//     alert(first / second)
+// }
+// else if(oprt === "%"){
+//     alert(first % second)
+// }
